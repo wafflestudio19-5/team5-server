c3eb5856d54de57c8178aa2c5d6971b643c99ff3
yjeong-k <kyj219@snu.ac.kr>
2021-11-23 05:06:39 +0900

    Complete user migration and write drafts of other models

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user',
+    'board',
+    'post',
+    'comment',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +127,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
--- everytime/post/models.py
+++ everytime/post/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from board.models import Board
+from everytime import settings
+
+
+class Post(models.Model):
+    pass
+#     # 게시판이 삭제되면 글도 삭제되어야하므로 cascade
+#     board = models.ForeignKey(Board, on_delete=models.CASCADE)
+#     # 작성자가 탈퇴해도 글은 남아있으므로 set_null, 작성자가 없으면 (알수없음)으로 뜸
+#     writer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+#
+#     title = models.CharField(max_length=100)    # 게시판에 따라 title 유무가 다름 --> 어떻게 구현?
+#     content = models.TextField()
+#     image = models.ImageField(upload_to='post/', null=True, blank=True)  # 사진은 필수가 아님, static/images/post에 저장됨
+#     # 게시판에 따라 image 첨부가능 여부가 다름 --> 어떻게 구현?
+#
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     num_of_likes = models.PositiveIntegerField(default=0, blank=True)
+#     num_of_scrap = models.PositiveIntegerField(default=0, blank=True)
+#     is_anonymous = models.BooleanField(default=True, blank=True)
+#     is_question = models.BooleanField(default=False, blank=True)
+#
