717e645cc5ce5c78261d39044bbce22994221fe2
성복 <tjdqhrdkdl@snu.ac.kr>
2021-11-24 10:46:49 +0900

    fix: static_dir add

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = STATIC_DIR
 
 MEDIA_URL = '/images/'
 
