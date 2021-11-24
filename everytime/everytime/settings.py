6eaa3c4c2f1c0381ea4fe7af2bca0258e1addfb7
성복 <tjdqhrdkdl@snu.ac.kr>
2021-11-24 21:05:40 +0900

    feat: swagger add

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_jwt',
     'rest_framework.authtoken',
-    'def_yasg',
+    'drf_yasg',
     'user',
     'board',
     'post',
--- everytime/everytime/urls.py
+++ everytime/everytime/urls.py
@@ -14,15 +14,46 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.conf.urls import url
+from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls'))
 ]
+if settings.DEBUG_TOOLBAR:
+    import debug_toolbar
 
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
 # profile picture 관련
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# swagger(api 문서화)
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Swagger Study API",
+        default_version="v1",
+        description="Swagger Study를 위한 API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="test", email="test@test.com"),
+        license=openapi.License(name="Test License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
