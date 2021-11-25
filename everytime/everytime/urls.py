c2a50925530dea3b77fcea6bb1d977d192865ff9
minkyu <whizkyu@snu.ac.kr>
2021-11-25 22:45:55 +0900

    user login, signup api 앞부분에 "user/" 추가

--- everytime/everytime/urls.py
+++ everytime/everytime/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('user.urls'))
+    path('user/', include('user.urls'))
 ]
 
 # profile picture 관련
