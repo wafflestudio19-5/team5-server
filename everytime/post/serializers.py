000391dd5283788bcf2bf0ee1c7cad43de0def51
성복 <tjdqhrdkdl@snu.ac.kr>
2021-12-18 06:05:36 +0900

    feat: Add feature accepting multiple images for post

--- everytime/post/serializers.py
+++ everytime/post/serializers.py
@@ -1,13 +1,23 @@
 from rest_framework import serializers, exceptions
-from .models import Post, Tag
+from .models import Post, Tag, PostImage
 from board.models import Board
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = PostImage
+        fields = ['image']
+
 
 class PostSerializer(serializers.ModelSerializer):
     board = serializers.StringRelatedField(read_only=True)
     writer = serializers.StringRelatedField(read_only=True)
     title = serializers.CharField(required=False, max_length=100)
     content = serializers.CharField()
-    image = serializers.ImageField(required=False)
+    images = serializers.SerializerMethodField()
     is_anonymous = serializers.BooleanField(default=False)
     is_question = serializers.BooleanField(default=False)
 
@@ -18,19 +28,29 @@ class PostSerializer(serializers.ModelSerializer):
             'writer',
             'title',
             'content',
-            'image',
+            'images',
             'is_anonymous',
             'is_question',
         )
+    
+    def get_images(self, obj):
+        image = obj.postimage_set.all()
+        return PostImageSerializer(instance=image, many=True).data
 
     def validate(self, data):
         user = self.context['request'].user
-        board = self.context['request'].query_params['board']
-        board = Board.objects.get(title=board)
+        try:
+            board = self.context['request'].query_params['board']
+        except:
+            raise serializers.ValidationError("board를 query parameter로 입력해주세요")
+        board = Board.objects.get(id=board)
         data['writer'] = user
         data['board'] = board
         return data
-
+    
     def create(self, validated_data):
         post = Post.objects.create(**validated_data)
+        image_set = self.context['request'].FILES
+        for image_data in image_set.getlist('image'):
+            PostImage.objects.create(post=post, image=image_data)
         return post
