a2e35866d4659e22883763d90dae0628b30e0bbb
minkyu <whizkyu@snu.ac.kr>
2021-12-18 22:46:40 +0900

    게시글 tag기능 추가 및 게시글 수정 기능 구현

--- everytime/post/serializers.py
+++ everytime/post/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers, exceptions
-from rest_framework.utils import model_meta
 from .models import Post, Tag, PostImage
 from board.models import Board
 
@@ -29,6 +28,7 @@ class PostSerializer(serializers.ModelSerializer):
             'writer',
             'title',
             'content',
+            'tags',
             'images',
             'is_anonymous',
             'is_question',
@@ -39,42 +39,61 @@ class PostSerializer(serializers.ModelSerializer):
         return PostImageSerializer(instance=image, many=True).data
 
     def validate(self, data):
-        user = self.context['request'].user
+        request = self.context['request']
+        user = request.user
+
         try:
-            board = self.context['request'].query_params['board']
+            board =request.query_params['board']
         except:
             raise serializers.ValidationError("board를 query parameter로 입력해주세요")
         board = Board.objects.get(id=board)
+
         data['writer'] = user
         data['board'] = board
         return data
     
     def create(self, validated_data):
-        post = Post.objects.create(**validated_data)
+        # tags 따로 저장하기
+        tags = validated_data.pop('tags') if 'tags' in validated_data else None
+
+        # 게시글 생성
+        try:
+            post = Post.objects.create(**validated_data)
+        except TypeError:
+            raise exceptions.ValidationError('올바르지 않은 타입을 입력하셨습니다.')
+
+        # 게시글과 태그 연결
+        if tags is not None:
+            for tag in tags:
+                post.tags.add(tag)
+
+        # 이미지 저장 및 업로드
         image_set = self.context['request'].FILES
         for image_data in image_set.getlist('image'):
             PostImage.objects.create(post=post, image=image_data)
+
         return post
 
     def update(self, post, validated_data):
-        PostImage.objects.filter(post=post).delete()
+        # 기존 이미지 삭제 후 교체 -> 근데 변경된 이미지만 부분적으로 바꾸거나 추가하는 방법이 있을까?
         image_set = self.context['request'].FILES
-        for image_data in image_set.getlist('image'):
-            PostImage.objects.create(post=post, image=image_data)
+        if image_set is not None:
+            PostImage.objects.filter(post=post).delete()
+            for image_data in image_set.getlist('image'):
+                PostImage.objects.create(post=post, image=image_data)
 
-        info = model_meta.get_field_info(post)
-        m2m_fields = []
+        # 기존 태그에서 사라진 부분은 제거 후 새로 생긴 태그 추가
+        new_tags = validated_data.pop('tags') if 'tags' in validated_data else None
+        if new_tags is not None:
+            post.tags.exclude(tag__in=[tag.tag for tag in new_tags]).delete()
+            past_tags = post.tags.all()
+            for tag in new_tags:
+                if tag not in past_tags:
+                    post.tags.add(tag)
 
+        # 입력된 값들에 한해서 기존 게시글 수정 (e.g. title key가 없으면 제목은 수정 안 함)
         for attr, value in validated_data.items():
-            if attr in info.relations and info.relations[attr].to_many:
-                m2m_fields.append((attr, value))
-            else:
-                setattr(post, attr, value)
-
+            setattr(post, attr, value)
         post.save()
-
-        for attr, value in m2m_fields:
-            field = getattr(post, attr)
-            field.set(value)
 
         return post
--- everytime/post/views.py
+++ everytime/post/views.py
@@ -10,6 +10,15 @@ from .serializers import PostSerializer
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
+# request.data안에 새로운 Tag를 찾아서 데이터베이스에 저장
+def create_tag(data):
+    if 'tags' in data:
+        for tag_name in data.getlist('tags'):
+            tag_name = tag_name.upper()
+            if not Tag.objects.filter(tag__iexact=tag_name).exists():
+                Tag.objects.create(tag=tag_name)
+
+
 class PostViewSet(viewsets.GenericViewSet):
     permission_classes = (permissions.IsAuthenticated, )
     serializer_class = PostSerializer
@@ -17,6 +26,9 @@ class PostViewSet(viewsets.GenericViewSet):
 
     def create(self, request):
         data = request.data
+
+        create_tag(data)
+
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         post = serializer.save()
@@ -33,12 +45,14 @@ class PostViewSet(viewsets.GenericViewSet):
 
     def update(self, request, pk=None):
         post = get_object_or_404(Post, pk=pk)
-
         data = request.data
         user = request.user
 
+        create_tag(data)
+
         if post.writer_id is not user.id:
             raise exceptions.AuthenticationFailed('글 작성자가 아니므로 글을 수정할 수 없습니다.')
+
         serializer = self.get_serializer(post, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
