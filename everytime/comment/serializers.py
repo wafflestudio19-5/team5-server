d4f66c4f5d576ac8dce50684316005b2acc9def4
yjeong-k <kyj219@snu.ac.kr>
2021-12-24 04:32:39 +0900

    Rought draft of comment implementation NOT DONE YET

--- everytime/comment/serializers.py
+++ everytime/comment/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers, exceptions
 from .models import Comment
 
+
 class CommentSerializer(serializers.ModelSerializer):
     writer = serializers.StringRelatedField(read_only=True)
     content = serializers.CharField()
@@ -12,6 +13,6 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = '__all__'
 
-    # def create(self, validated_data):
-        # comment = Comment.objects.create(**validated_data)
-        # return comment
+    def create(self, validated_data):
+        comment = Comment.objects.create(**validated_data)
+        return comment
--- everytime/comment/views.py
+++ everytime/comment/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions, status
+from django.shortcuts import get_object_or_404
+from comment.models import Comment
+from comment.serializers import CommentSerializer
+
+
+class CommentViewSet(viewsets.GenericViewSet):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        comment = serializer.save()
+        return Response(self.get_serializer(comment).data, status=status.HTTP_201_CREATED)
+
+    # 댓글 수정 불가
+
+    def list(self, request):
+        post = request.query_params.get('post')
+        comments = self.get_queryset().filter(post_id=post).all()
+        return Response(self.get_serializer(comments, many=True).data)
+
+    def destroy(self, request, pk=None):
+        comment = get_object_or_404(Comment, pk=pk)
+        comment.is_deleted = True
+        comment.save()
+        return Response("%s번 댓글이 삭제되었습니다." % pk, status=status.HTTP_200_OK)
