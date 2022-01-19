848a3ebeb99c5cdfc89d6adf0133c1cb0f091544
minkyu <whizkyu@snu.ac.kr>
2022-01-20 02:11:33 +0900

    친구 수락 viewset -> view로 수정 (url 선정이 더 자유로운 것 같음) + 친구 요청 삭제, 친구 삭제 API 분리+ 친구 API 테스트 완료

--- everytime/friend/views.py
+++ everytime/friend/views.py
@@ -1,39 +1,80 @@
-from rest_framework import viewsets, views, permissions, status
+from django.db.transaction import atomic
+from rest_framework import viewsets, permissions, status, generics
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Friend
+from user.models import User
+from .models import Friend, FriendRequest
 from .serializers import FriendRequestSerializer, FriendSerializer
+from everytime.exceptions import FieldError, ServerError, NotFound
 
 # Create your views here.
-class FreindRequestView(views.APIView):
+class FriendRequestView(generics.GenericAPIView):
     permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = FriendRequestSerializer
 
     def post(self, request):
         data = request.data
-        user = request.user
-        data['sender'] = user.nickname or None
-        serializer = FriendRequestSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        friend_request = serializer.save()
-        return Response(FriendRequestSerializer(friend_request).data, status=status.HTTP_201_CREATED) # 이 부분 직접 친구신청 해보고 response 수정해야됨
-
-class FriendViewset(viewsets.GenericViewSet):
-    permission_classes = (permissions.IsAuthenticated, )
-    serializer_class = FriendSerializer
-
-    def create(self, request):
-        data = request.data
-        user = request.user
-        data['user'] = user.nickname or None
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
         friend_request = serializer.save()
-        queryset = Friend.objects.filter(user=user)
-        return Response(self.get_serializer(queryset, many=True).data, status=status.HTTP_201_CREATED)
-
-    def list(self, request):
-        user = request.user
-        queryset = Friend.objects.filter(user=user)
-        return Response(self.get_serializer(queryset, many=True).data, status=status.HTTP_200_OK)
+        if friend_request is None:
+            raise ServerError()
+        return Response("친구 요청을 보냈습니다.\n상대방이 수락하면 친구가 맺어짖니다.", status=status.HTTP_201_CREATED) # 이 부분 직접 친구신청 해보고 response 수정해야됨
 
     def delete(self, request):
-        pass
+        data = request.data
+        user = request.user
+        sender = data.get('sender', None)
+        if sender is None:
+            raise FieldError()
+        try:
+            delete_request = FriendRequest.objects.get(sender=sender, receiver=user)
+        except:
+            raise ServerError()
+        delete_request.delete()
+        return Response("친구 요청을 거절하였습니다.", status=status.HTTP_200_OK)
+
+
+
+class FriendView(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = FriendSerializer
+
+    def post(self, request, pk=None):
+        user = request.user
+        if not pk:
+            raise FieldError('친구 신청을 확인해주세요.')
+        serializer = self.get_serializer(data={'friend': pk})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response("친구 요청을 수락하였습니다.", status=status.HTTP_201_CREATED)
+
+    def delete(self, request, pk=None):
+        user = request.user
+        if not pk:
+            raise FieldError('삭제할 친구를 입력해주세요.')
+        try:
+            friend = User.objects.get(pk=pk)
+            relation1 = Friend.objects.get(user=user, friend=friend)
+            relation2 = Friend.objects.get(user=friend, friend=user)
+        except:
+            raise NotFound('존재하지 않는 친구입니다.')
+        with atomic():
+            relation1.delete()
+            relation2.delete()
+        return Response("친구를 삭제하였습니다.", status=status.HTTP_200_OK)
+
+class FriendListView(generics.GenericAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
+    serializer_class = FriendSerializer
+
+    def get(self, request):
+        user = request.user
+        queryset = Friend.objects.filter(user=user)
+        request_queryset = FriendRequest.objects.filter(receiver=user)
+        request_exist = request_queryset.exists()
+        return Response({
+            "request_exist": request_exist,
+            "request_list": request_queryset.values('sender__id', 'sender__nickname'),
+            "friend_list": queryset.values('friend__id', 'friend__nickname')
+        }, status=status.HTTP_200_OK)
