fe5bbcff4434c8b1ed3694327d8e8a1d94a374ca
성복 <tjdqhrdkdl@snu.ac.kr>
2022-01-19 16:14:39 +0900

    fix: change jwt token lifetime

--- everytime/everytime/settings/base.py
+++ everytime/everytime/settings/base.py
@@ -199,7 +199,7 @@ REST_FRAMEWORK = {
 # }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1000),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
 
     'ALGORITHM': 'HS256',
