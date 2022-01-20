85d6831957844226f199a2db42202c4a025db16c
성복 <tjdqhrdkdl@snu.ac.kr>
2022-01-20 22:06:42 +0900

    fix: lecture_search

--- everytime/lecture/filters.py
+++ everytime/lecture/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 from django.forms.fields import MultipleChoiceField, CharField
-from django.db.models import Q, F
+from django.db.models import Q, F, Count
 
 from everytime.exceptions import FieldError
 from .models import Lecture, Department, LectureTime
@@ -17,6 +17,54 @@ CLASSIFICATION_CHOICES = (
     ('교직', '교직'),
     ('공통', '공통'),
 )
+
+
+class LectureFilter(django_filters.FilterSet):
+    grade = django_filters.MultipleChoiceFilter(choices=GRADE_CHOICES)
+    classification = django_filters.MultipleChoiceFilter(choices=CLASSIFICATION_CHOICES)
+    credits = NumInFilter()
+    department = django_filters.ModelChoiceFilter(field_name='course__department',to_field_name='name', queryset=Department.objects.all())
+    title = django_filters.CharFilter(field_name='course__title', lookup_expr='icontains', min_length=2)
+    instructor = django_filters.CharFilter(field_name='course__instructor', lookup_expr='icontains', min_length=2)
+    course_code = django_filters.CharFilter(field_name='course_code', lookup_expr='icontains')
+    location = django_filters.CharFilter(field_name='lecturetime__location', lookup_expr='icontains')
+    lecturetime = MultipleValueFilter(field_class=CharField, method='custom_time_filter')
+    ordering = UserOrderingFilter(
+        fields=(
+            ('cart', 'cart'),
+            ('course_code', 'course_code'),
+        ),
+    )
+
+    class Meta:
+        model = Lecture
+        fields = {
+            'classification':['exact'],
+            'grade':['exact'],
+            'credits':['in']
+        }
+
+    def custom_time_filter(self, queryset, value, *arg):
+        q = Q()
+        time_queryset = LectureTime.objects.filter(lecture__course__self_made=False)\
+                        .select_related('lecture__course')
+        for time_string in arg[0]:
+            if len(time:=time_string.split('/')) != 3:
+                raise FieldError('시간값이 요일/시작/끝 의 형식과 맞지 않습니다.')
+            day = time[0]
+            start = int(time[1])
+            end = int(time[2])
+
+            q.add(
+                Q(day=day)&
+                Q(start__gte=start)&
+                Q(end__lte=end),
+            q.OR)
+        lecture_set = time_queryset.filter(~q).all().values('lecture')
+        queryset = queryset.exclude(id__in=lecture_set)\
+                .exclude(lecturetime=None)
+
+        return queryset
 
 class MultipleValueField(MultipleChoiceField):
     def __init__(self, *args, field_class, **kwargs):
@@ -42,14 +90,27 @@ class NumInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
     pass
 
 
-class NullsAlwaysLastOrderingFilter(django_filters.OrderingFilter):
+class UserOrderingFilter(django_filters.OrderingFilter):
     """ Use Django 1.11 nulls_last feature to force nulls to bottom in all orderings. """
-    def filter_queryset(self, request, queryset, view):
-        ordering = self.get_ordering(request, queryset, view)
 
-        if ordering:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 모델에 없는 필드를 허용하기 위해 여기에서 정보를 전달합니다.
+        self.extra['choices'] += [
+            ('competition', '-competition'),
+            ('-competition', 'competition'),
+            ('title','title'),
+        ]
+
+
+    def filter(self, queryset, value):
+        if value and value[0] in ['competition', '-competition']:
+            queryset = queryset.annotate(competition=F('cart')/F('quota'))
+        if value and value[0] == 'title':
+            value[0] = 'course__title'
+        if value:
             f_ordering = []
-            for o in ordering:
+            for o in value:
                 if not o:
                     continue
                 if o[0] == '-':
@@ -61,49 +122,3 @@ class NullsAlwaysLastOrderingFilter(django_filters.OrderingFilter):
 
         return queryset
 
-class LectureFilter(django_filters.FilterSet):
-    grade = django_filters.MultipleChoiceFilter(choices=GRADE_CHOICES)
-    classification = django_filters.MultipleChoiceFilter(choices=CLASSIFICATION_CHOICES)
-    credits = NumInFilter()
-    department = django_filters.ModelChoiceFilter(field_name='course__department',to_field_name='name', queryset=Department.objects.all())
-    title = django_filters.CharFilter(field_name='course__title', lookup_expr='icontains', min_length=2)
-    instructor = django_filters.CharFilter(field_name='course__instructor', lookup_expr='icontains', min_length=2)
-    course_code = django_filters.CharFilter(field_name='course_code', lookup_expr='icontains')
-    location = django_filters.CharFilter(field_name='lecturetime__location', lookup_expr='icontains')
-    lecturetime = MultipleValueFilter(field_class=CharField, method='custom_time_filter')
-    ordering = NullsAlwaysLastOrderingFilter(
-        fields=(
-            ('course_code', 'course_code'),
-            ('title', 'course__title'),
-            ('instructor', 'course__title'),
-            ('cart', 'cart'),
-        )
-    )
-
-    class Meta:
-        model = Lecture
-        fields = {
-            'classification':['exact'],
-            'grade':['exact'],
-            'credits':['in']
-        }
-
-    def custom_time_filter(self, queryset, value, *arg):
-        q = Q()
-        time_queryset = LectureTime.objects.filter(lecture__course__self_made=False)\
-                        .all().select_related('lecture__course')
-        for time_string in arg[0]:
-            if len(time:=time_string.split('/')) != 3:
-                raise FieldError('시간값이 요일/시작/끝 의 형식과 맞지 않습니다.')
-            day = time[0]
-            start = int(time[1])
-            end = int(time[2])
-
-            q.add(
-                Q(day=day)&
-                Q(start__gte=start)&
-                Q(end__lte=end),
-            q.OR)
-        lecture_set = time_queryset.filter(~q).all().values('lecture')
-        queryset = queryset.exclude(id__in=lecture_set)
-        return queryset
