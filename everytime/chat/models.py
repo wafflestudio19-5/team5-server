da52c1af2cfb9327cdb4eb7f2e98ca774b242991
minkyu <whizkyu@snu.ac.kr>
2022-01-23 05:53:38 +0900

    is_anonymous 필드 추가

--- everytime/chat/models.py
+++ everytime/chat/models.py
@@ -4,14 +4,15 @@ from django.db import models
 class ChatRoom(models.Model):
     user = models.ForeignKey('user.User', related_name='my_chatrooms', on_delete=models.CASCADE)
     partner = models.ForeignKey('user.User', related_name='other_chatrooms', on_delete=models.SET_NULL, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_anonymous = models.BooleanField(default=False)
 
 
 class Message(models.Model):
     sender = models.ForeignKey('user.User', related_name='sent_messages', on_delete=models.SET_NULL, null=True)
     receiver = models.ForeignKey('user.User', related_name='received_messages', on_delete=models.SET_NULL, null=True)
     content = models.TextField(null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
 
 class ChatRoomMessage(models.Model):
