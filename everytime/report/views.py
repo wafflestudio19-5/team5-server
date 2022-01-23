b2cf4aaedf46fedb4e6511c6f3513926e56e3b06
yjeong-k <kyj219@snu.ac.kr>
2022-01-23 16:08:51 +0900

    report views에서 importing하는 serializer 추가

--- everytime/report/views.py
+++ everytime/report/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from report.models import ReportedUser
-from report.serializers import PostReportSerializer
+from report.serializers import PostReportSerializer, CommentReportSerializer, EvaluationReportSerializer, \
+    ExamInfoReportSerializer
 
 
 class PostReportView(APIView):
