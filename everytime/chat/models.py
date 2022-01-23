d8dd8cd92e41a58ab3550e9e78c7252cb7ee031d
minkyu <whizkyu@snu.ac.kr>
2022-01-23 16:11:38 +0900

    대화상대가 동일해도 is_anonymous가 다르면 다른 채팅방으로 분류

--- everytime/chat/models.py
+++ everytime/chat/models.py
@@ -10,7 +10,7 @@ class ChatRoom(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "partner"],
+                fields=["user", "partner", "is_anonymous"],
                 name="chat room"
             )
         ]
@@ -20,9 +20,15 @@ class Message(models.Model):
     sender = models.ForeignKey('user.User', related_name='sent_messages', on_delete=models.SET_NULL, null=True)
     receiver = models.ForeignKey('user.User', related_name='received_messages', on_delete=models.SET_NULL, null=True)
     content = models.TextField(null=False)
+    is_notice = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
 
 class ChatRoomMessage(models.Model):
-    chatroom = models.ForeignKey('ChatRoom', related_name='message_set', on_delete=models.SET_NULL, null=True)
+    chatroom = models.ForeignKey('ChatRoom', related_name='message_set', on_delete=models.CASCADE)
     message = models.ForeignKey('Message', related_name='chatroom_set', on_delete=models.CASCADE)
+
+
+class ChatBlackList(models.Model):
+    user = user = models.ForeignKey('user.User', related_name='my_blacklist', on_delete=models.CASCADE)
+    partner = models.ForeignKey('user.User', related_name='other_blacklist', on_delete=models.CASCADE)
