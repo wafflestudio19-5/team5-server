44129bf28349f1f3f0f9158562bbe6924ac2489b
minkyu <whizkyu@snu.ac.kr>
2022-01-26 02:05:50 +0900

    IsAuthenticated 재정의

---
+++ everytime/everytime/permissions.py
@@ -0,0 +1,17 @@
+from django.utils import timezone
+from datetime import timedelta
+from rest_framework.permissions import BasePermission, AllowAny
+from report.models import ReportedUser
+
+
+class IsAuthenticated(BasePermission):
+    def has_permission(self, request, view):
+        if bool(request.user and request.user.is_authenticated):
+            reported_user = ReportedUser.objects.filter(school_email=request.user.school_email)
+            if reported_user.exists():
+                reported_user = reported_user[0]
+                return timezone.now() > reported_user.updated_at + timedelta(days=30)
+            else:
+                return True
+        else:
+            return False
